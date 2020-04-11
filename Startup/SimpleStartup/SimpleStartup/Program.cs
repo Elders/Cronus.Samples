08246e95b522d8f439fb78f2ba7d4a760f55c7fe
Konstantin Tenekedzhiev <konstantin.tenekedzhiev@eldersoss.com>
2020-04-11 20:50:06 +0300

    Basic Cronus configuration with json

---
+++ Startup/SimpleStartup/SimpleStartup/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Elders.Cronus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleStartup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var services = new ServiceCollection();
+            services.AddCronus(configuration);
+
+            var serviceProvider = services.BuildServiceProvider();
+            var cronusHost = serviceProvider.GetRequiredService<ICronusHost>();
+            cronusHost.Start();
+        }
+    }
+}
